406cc01dbdb8653576cab1208f3461961ec91152
delskull <Deluta1989@gmail.com>
2024-03-21 15:50:33 +0300

    fix: repare code

--- src/index.js
+++ src/index.js
@@ -26,15 +26,19 @@ module.exports = function toReadable(number) {
     if (number.toString().length === 2 && (!number.toString() % 10 === 0)){
       return tens[Math.floor(number / 10)] + ' ' + ones[number % 10 - 1]
     }
-    // if (number.toString().length === 3 && number.toString() % 100 === 0){
-    //   return ones[number / 100 - 1] + ' ' + 'hundred'
-    // }
+    if (number.toString().length === 3 && number.toString() % 100 === 0){
+      return ones[number / 100 - 1] + ' ' + 'hundred'
+    }
     if (number.toString().length === 3 && number.toString() % 10 === 0){
-      return ones[Math.floor(number / 100 - 1)] + '  ' + 'hundred' + '  ' + tens[Math.floor(number % 100 / 10)]
+      return ones[Math.floor(number / 100 - 1)] + ' ' + 'hundred' + ' ' + tens[Math.floor(number % 100 / 10)]
     }
     if (number.toString().length === 3 && (!number.toString() % 10 === 0) && (number % 100) > 10 && (number % 100) < 20){
+      console.log(number % 100 )
+      return ones[Math.floor(number / 100 - 1)] + ' ' + 'hundred' + ' ' + teens[Math.floor(number % 100) - 10]
+    }
+    if (number.toString().length === 3 && (!number.toString() % 10 === 0) && (number % 100) < 10 && (number % 100) >= 1){
       // console.log(number % 100 )
-      return ones[Math.floor(number / 100 - 1)] + '  ' + 'hundred' + '  ' + teens[Math.floor(number % 100) - 10]
+      return ones[Math.floor(number / 100 - 1)] + ' ' + 'hundred' + ' ' + ones[number % 10  - 1]
     }
     if (number.toString().length === 3 && (!number.toString() % 10 === 0)){
       // console.log(number % 100)
